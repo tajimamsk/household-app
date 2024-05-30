7cee3b0dc066844fe885a7f2eac67c8cbb59ad30
tajimamsk <masaki.tajima@g-seed.co.jp>
2024-05-30 17:46:01 +0900

    ファイルの準備

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Report from "./pages/Report";
+import NoMatch from "./pages/NoMatch";
+import AppLayout from "./components/layout/AppLayout";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/report" element={<Report />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
