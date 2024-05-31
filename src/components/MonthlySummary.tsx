4c18a999e2db04f45d26a3af2a982b851dfa16a2
tajimamsk <masaki.tajima@g-seed.co.jp>
2024-05-31 15:14:10 +0900

    みため

---
+++ src/components/MonthlySummary.tsx
@@ -0,0 +1,102 @@
+import { Card, CardContent, Grid, Stack, Typography } from "@mui/material";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import AccountbalanceIcon from "@mui/icons-material/AccountBalance";
+
+const MonthlySummary = () => {
+  return (
+    <Grid container spacing={{ xs: 1, sm: 2 }} mb={2}>
+      {/* 収入 */}
+      <Grid item xs={4} display={"flex"} flexDirection={"column"}>
+        <Card
+          sx={{
+            bgcolor: "blue",
+            color: "white",
+            borderRadius: "10px",
+            flexGrow: "1",
+          }}
+        >
+          <CardContent sx={{ padding: { xs: 1, sm: 2 } }}>
+            <Stack direction={"row"}>
+              <ArrowUpwardIcon sx={{ fontSize: "2rem" }} />
+              <Typography>収入</Typography>
+            </Stack>
+            <Typography
+              textAlign={"right"}
+              variant="h5"
+              fontWeight={"fontWeightBold"}
+              sx={{
+                wordBreak: "break-word",
+                fontSize: { xs: ".8rem", sm: "1rem", md: "1.2rem" },
+              }}
+            >
+              ¥300
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+
+      {/* 支出 */}
+      <Grid item xs={4} display={"flex"} flexDirection={"column"}>
+        <Card
+          sx={{
+            bgcolor: "red",
+            color: "white",
+            borderRadius: "10px",
+            flexGrow: "1",
+          }}
+        >
+          <CardContent sx={{ padding: { xs: 1, sm: 2 } }}>
+            <Stack direction={"row"}>
+              <ArrowDownwardIcon sx={{ fontSize: "2rem" }} />
+              <Typography>支出</Typography>
+            </Stack>
+            <Typography
+              textAlign={"right"}
+              variant="h5"
+              fontWeight={"fontWeightBold"}
+              sx={{
+                wordBreak: "break-word",
+                fontSize: { xs: ".8rem", sm: "1rem", md: "1.2rem" },
+              }}
+            >
+              ¥300
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+
+      {/* 残高 */}
+      <Grid item xs={4} display={"flex"} flexDirection={"column"}>
+        <Card
+          sx={{
+            bgcolor: "green",
+            color: "white",
+            borderRadius: "10px",
+            flexGrow: "1",
+          }}
+        >
+          <CardContent sx={{ padding: { xs: 1, sm: 2 } }}>
+            <Stack direction={"row"}>
+              <AccountbalanceIcon sx={{ fontSize: "2rem" }} />
+              <Typography>残高</Typography>
+            </Stack>
+            <Typography
+              textAlign={"right"}
+              variant="h5"
+              fontWeight={"fontWeightBold"}
+              sx={{
+                wordBreak: "break-word",
+                fontSize: { xs: ".8rem", sm: "1rem", md: "1.2rem" },
+              }}
+            >
+              ¥300
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default MonthlySummary;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { Box } from "@mui/material";
+import MonthlySummary from "../components/MonthlySummary";
+import Calendar from "../components/Calendar";
+import TransactionForm from "../components/TransactionForm";
+import TransactionMenu from "../components/TransactionMenu";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box sx={{ display: "flex" }}>
+      {/* 左 */}
+      <Box sx={{ flexGrow: 1 }}>
+        <MonthlySummary />
+        <Calendar />
+      </Box>
+
+      {/* 右 */}
+      <Box>
+        <TransactionMenu />
+        <TransactionForm />
+      </Box>
+    </Box>
+  );
 };
 
 export default Home;
