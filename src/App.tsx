11102b95cacd70e4f05fbe6a64ec88726d0838dc
tajimamsk <masaki.tajima@g-seed.co.jp>
2024-05-31 14:15:49 +0900

    レイアウト

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,23 @@ import Home from "./pages/Home";
 import Report from "./pages/Report";
 import NoMatch from "./pages/NoMatch";
 import AppLayout from "./components/layout/AppLayout";
+import { theme } from "./theme/theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<AppLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/report" element={<Report />} />
-          <Route path="*" element={<NoMatch />} />
-        </Route>
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/report" element={<Report />} />
+            <Route path="*" element={<NoMatch />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/common/SideBar.tsx
@@ -0,0 +1,134 @@
+import {
+  Box,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+} from "@mui/material";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import HomeIcon from "@mui/icons-material/Home";
+import EqualizerIcon from "@mui/icons-material/Equalizer";
+import { NavLink } from "react-router-dom";
+import { CSSProperties } from "react";
+
+interface SideBarProps {
+  drawerWidth: number;
+  mobileOpen: boolean;
+  handleDrawerTransitionEnd: () => void;
+  handleDrawerClose: () => void;
+}
+
+interface menuItem {
+  text: string;
+  path: string;
+  icon: React.ComponentType;
+}
+
+const SideBar = ({
+  drawerWidth,
+  mobileOpen,
+  handleDrawerTransitionEnd,
+  handleDrawerClose,
+}: SideBarProps) => {
+  const MenuItems: menuItem[] = [
+    {
+      text: "Home",
+      path: "/",
+      icon: HomeIcon,
+    },
+    { text: "Report", path: "/report", icon: EqualizerIcon },
+  ];
+
+  // リンクテキストの下線を消す
+  const baseLinkStyle: CSSProperties = {
+    textDecoration: "none",
+    color: "inherit",
+    display: "block",
+  };
+
+  const activeLinkStyle: CSSProperties = {
+    backgroundColor: "rgba(0,0,0,0.08)",
+  };
+
+  const drawer = (
+    <div>
+      <Toolbar />
+      <Divider />
+      <List>
+        {MenuItems.map((item, index) => (
+          <NavLink
+            to={item.path}
+            style={({ isActive }) => {
+              console.log("選択されたメニューは", item.text, isActive);
+              return {
+                ...baseLinkStyle,
+                ...(isActive ? activeLinkStyle : {}),
+              };
+            }}
+            key={index}
+          >
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
+                  <item.icon />
+                </ListItemIcon>
+                <ListItemText primary={item.text} />
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <Box
+      component="nav"
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+      aria-label="mailbox folders"
+    >
+      {/* for mobile */}
+      <Drawer
+        variant="temporary"
+        open={mobileOpen}
+        onTransitionEnd={handleDrawerTransitionEnd}
+        onClose={handleDrawerClose}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+        sx={{
+          display: { xs: "block", sm: "none" },
+          "& .MuiDrawer-paper": {
+            boxSizing: "border-box",
+            width: drawerWidth,
+          },
+        }}
+      >
+        {drawer}
+      </Drawer>
+
+      {/* for PC */}
+      <Drawer
+        variant="permanent"
+        sx={{
+          display: { xs: "none", sm: "block" },
+          "& .MuiDrawer-paper": {
+            boxSizing: "border-box",
+            width: drawerWidth,
+          },
+        }}
+        open
+      >
+        {drawer}
+      </Drawer>
+    </Box>
+  );
+};
+
+export default SideBar;
