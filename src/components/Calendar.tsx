5ff3a20b3698dd9698704f15b92ee3760c2b29cd
tajimamsk <masaki.tajima@g-seed.co.jp>
2024-06-03 10:08:42 +0900

    カレンダー表示

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,7 +1,45 @@
+import FullCalendar from "@fullcalendar/react";
 import React from "react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import jaLocale from "@fullcalendar/core/locales/ja";
+import "../calendar.css";
+import { EventContentArg } from "@fullcalendar/core";
 
 const Calendar = () => {
-  return <div>Calendar</div>;
+  const events = [
+    {
+      title: "Meeting",
+      start: new Date(),
+      income: 500,
+      expense: 400,
+      balance: 100,
+    },
+  ];
+  const renderEventContent = (eventInfo: EventContentArg) => {
+    console.log(eventInfo);
+    return (
+      <div>
+        <div className="money" id="event-income">
+          {eventInfo.event.extendedProps.income}
+        </div>
+        <div className="money" id="event-expense">
+          {eventInfo.event.extendedProps.expense}
+        </div>
+        <div className="money" id="event-balance">
+          {eventInfo.event.extendedProps.balance}
+        </div>
+      </div>
+    );
+  };
+  return (
+    <FullCalendar
+      locale={jaLocale}
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      events={events}
+      eventContent={renderEventContent}
+    />
+  );
 };
 
 export default Calendar;
