64edb6a87fc37fe190776e2e60c80c5a7c541eeb
tajimamsk <masaki.tajima@g-seed.co.jp>
2024-06-05 10:24:45 +0900

    スタイルの作成

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import MonthSelector from "../components/MonthSelector";
+import CategoryChart from "../components/CategoryChart";
+import BarChart from "../components/BarChart";
+import TransactionTable from "../components/TransactionTable";
+
+import { Grid, Paper } from "@mui/material";
 
 const Report = () => {
-  return <div>Report</div>;
+  const commonPaperStyle = {
+    height: { xs: "auto", md: "400px" },
+    display: "flex",
+    flexDirection: "column",
+  };
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        {/* 日付選択エリア */}
+        <MonthSelector />
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <Paper sx={commonPaperStyle}>
+          {/* 円グラフ */}
+          <CategoryChart />
+        </Paper>
+      </Grid>
+      <Grid item xs={12} md={8}>
+        <Paper sx={commonPaperStyle}>
+          {/* 棒グラフ */}
+          <BarChart />
+        </Paper>
+      </Grid>
+      <Grid item xs={12}>
+        {/* テーブル */}
+        <TransactionTable />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Report;
